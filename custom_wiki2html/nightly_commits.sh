ed7e8359bbe9e35c0241364298d4a7a24809dcf8
Aman Bhatia <aman.bhatia@outlook.com>
2017-10-07 01:36:37 -0700

    [Nightly Commit] 20171007T013637

--- custom_wiki2html/nightly_commits.sh
+++ custom_wiki2html/nightly_commits.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-shopt -s expand_aliases
-source ~/.bashrc
+
+TIMESTAMP=$( date +%Y%m%dT%H%M%S )
+echo ${TIMESTAMP}
 
 git add vimwiki_md/*.md
 git add vimwiki_md/*/*.md
 git add html/vimwiki_md/*.html
 git add html/vimwiki_md/*/*.html
-git commit -m "[Nightly Commit] `timestamp`"
-git push github
+git commit -m "[Nightly Commit] ${TIMESTAMP}"
+git push keybase
