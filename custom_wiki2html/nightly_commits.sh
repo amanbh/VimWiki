bd8f0822aa5810023ac0ac463d2544e00ff43388
Aman Bhatia <aman.bhatia@outlook.com>
2017-10-07 01:51:46 -0700

    Update paths in nightly_commit.sh

--- custom_wiki2html/nightly_commits.sh
+++ custom_wiki2html/nightly_commits.sh
@@ -3,9 +3,9 @@
 TIMESTAMP=$( date +%Y%m%dT%H%M%S )
 echo ${TIMESTAMP}
 
-git add vimwiki_md/*.md
-git add vimwiki_md/*/*.md
-git add html/vimwiki_md/*.html
-git add html/vimwiki_md/*/*.html
+git add markdown/*.md
+git add markdown/*/*.md
+git add generated_html/*.html
+git add generated_html/*/*.html
 git commit -m "[Nightly Commit] ${TIMESTAMP}"
 git push keybase
